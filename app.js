0a7e745581bc89be8a485e563bde04a2bbcdd064
Juan Esteban Narváez <90725923+JuanEstebanNarvaez@users.noreply.github.com>
2021-11-17 22:34:24 -0500

    Actualización app.js

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
+// Obtener la informacion del formulario, amañadiendo un listener cada vez que se presione el boton con tag submit ejecutando la función saveTask
 document.getElementById("formTask").addEventListener("submit",saveTask);
 
 function saveTask(e){
+    // Se obtienen los datos del formulario y se almacenan en variables separadas
     let title = document.getElementById('title').value;
     let fecha = document.getElementById('fecha').value;
     let hora = document.getElementById('hora').value;
     let description = document.getElementById('description').value;
-
+    //Objeto que contiene la información del formulario, se asigna el valor de las variables a las key
     const task = {
         title: title,
         description: description,
         fecha: fecha,
         hora: hora
     };
-
-    if(localStorage.getItem("tasks") === null){
-        let tasks = [];
-        tasks.push(task);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+    // Se hace uso de Local Storage para almacenar los datos dentro del navegador 
+    if(localStorage.getItem("tasks") === null){ // Se usa la funcion getItem para pedir una copia de la informacion del formulario
+        let tasks = []; // Creación del arreglo tasks (donde se almancenan las actividades)
+        tasks.push(task); // Se llena este arreglo con el metodo push con la información de los objetos
+        // JSON.stringify convierte el objeto a string para tener una mejor presentacion en el navegador
+        localStorage.setItem("tasks", JSON.stringify(tasks)); // setItem carga la información
 
     } else{
-        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        //JSON.parse convierte el string en JSON (objeto)
+        let tasks = JSON.parse(localStorage.getItem("tasks")); 
         tasks.push(task);
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }
 
     getTasks();
-    document.getElementById("formTask").reset();
-    e.preventDefault();
+    document.getElementById("formTask").reset(); // Limpia la informacion ingresada en el formulario
+    e.preventDefault(); // Previene el reseteo de la información cuando se refresque la página
 }
 
 function getTasks(){
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    let tasksView = document.getElementById("tasks");
+    let tasks = JSON.parse(localStorage.getItem("tasks")); //Obtencion de los datos
+    let tasksView = document.getElementById("tasks"); // Reconocimiento del div con id tasks
 
-    tasksView.innerHTML = '';
+    tasksView.innerHTML = ''; // Se inicializa vacio la variable
 
-    for(let i=0; i<tasks.length; i++){
+    for(let i=0; i<tasks.length; i++){ // Recorrido del arreglo tasks
+        // Extracción de la información de los arreglos
         let title = tasks[i].title;
         let description = tasks[i].description;
         let fecha = tasks[i].fecha;
         let hora = tasks[i].hora;
-
+        // Creación del div que presenta la información del formulario
         tasksView.innerHTML += `<div class="card mb-3">
             <div class="card-body">
                 <p>${title}  ${fecha} - ${hora} - ${description}</p> 
@@ -50,15 +55,15 @@ function getTasks(){
     }
 }
 
-function deleteTasks(title){
+function deleteTasks(title){ // Funcion para borrar cualquier tarea a partir del nombre de la actividad
     let tasks = JSON.parse(localStorage.getItem("tasks"));
     for(let i=0; i<tasks.length; i++){
-        if (tasks[i].title === title){
-            tasks.splice(i, 1);
+        if (tasks[i].title === title){ // Se busca que coincida el nombre de la tarea con el que pide la funcion
+            tasks.splice(i, 1); // Se quita el dato requerido
         }
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        getTasks();
     }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    getTasks();
 }
 
 getTasks();
